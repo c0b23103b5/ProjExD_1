f4a66d52642b927ee56ce93c1aa416f3978fac95
tori <c0b23103b5@edu.teu.ac.jp>
2024-11-19 16:58:28 +0900

    えんしゅう1

--- pygame_image.py
+++ pygame_image.py
@@ -20,7 +20,7 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        
+        x = tmr%3200
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
             kk_rct.move_ip(0,-1)
@@ -30,13 +30,13 @@ def main():
             kk_rct.move_ip(-1,0)
         if key_lst[pg.K_RIGHT]:
             kk_rct.move_ip(1,0)
-        
-        x = tmr%3200
+    
         screen.blit(bg_img, [-x, 0]) #screen Surfaceに背景画像Surfaceを貼り付ける
         screen.blit(bg_img_r, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0]) 
         screen.blit(bg_img_r, [-x+4800, 0])
-        screen.blit(kk_img, kk_rct)
+        screen.blit(kk_img, [-x+kk_rct[0], kk_rct[1]])
+        # screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
